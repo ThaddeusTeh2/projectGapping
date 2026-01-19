604cde9fa6a65af4c07baa18c66cc7673f9ac592
Thaddeus Teh :D <thaddeusteh2@gmail.com>
2026-01-19 14:29:43 +0800

    implement firebase preq integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -17,4 +22,7 @@ class MainApp extends StatelessWidget {
       ),
     );
   }
+
+  
 }
+
